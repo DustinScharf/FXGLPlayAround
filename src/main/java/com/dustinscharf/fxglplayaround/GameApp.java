56b901b6efcf21e84c10b190f959340e375d6fdc
DustinScharf <d.scharf.2002@gmail.com>
2021-08-30 22:35:48 +0200

    Initial commit with empty window

---
+++ src/main/java/com/dustinscharf/fxglplayaround/GameApp.java
@@ -0,0 +1,15 @@
+package com.dustinscharf.fxglplayaround;
+
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+
+public class GameApp extends GameApplication {
+    @Override
+    protected void initSettings(GameSettings gameSettings) {
+        // empty
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
