55ebb092cdaf148a72957016e89466b8b5a05e24
DustinScharf <d.scharf.2002@gmail.com>
2021-09-02 16:06:45 +0200

    Added loop spawning

--- src/main/java/com/dustinscharf/fxglplayaround/GameApp.java
+++ src/main/java/com/dustinscharf/fxglplayaround/GameApp.java
@@ -2,7 +2,12 @@ package com.dustinscharf.fxglplayaround;
 
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
+import com.almasb.fxgl.core.math.FXGLMath;
 import com.almasb.fxgl.dsl.FXGL;
+import javafx.geometry.Rectangle2D;
+import javafx.util.Duration;
+
+import static com.almasb.fxgl.dsl.FXGL.*;
 
 public class GameApp extends GameApplication {
     @Override
@@ -12,9 +17,17 @@ public class GameApp extends GameApplication {
 
     @Override
     protected void initGame() {
-        FXGL.getGameWorld().addEntityFactory(new Factory());
-        FXGL.spawn("enemy", 100, 100);
-        FXGL.spawn("ally", 600, 100);
+        getGameWorld().addEntityFactory(new Factory());
+
+        run(() -> {
+            spawn("enemy",
+                    FXGLMath.randomPoint(new Rectangle2D(0, 0, getAppWidth(), getAppHeight()))
+            );
+
+            spawn("ally",
+                    FXGLMath.randomPoint(new Rectangle2D(0, 0, getAppWidth(), getAppHeight()))
+            );
+        }, Duration.seconds(1));
     }
 
     public static void main(String[] args) {
