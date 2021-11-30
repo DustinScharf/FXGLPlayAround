85f727945116bbc06a8ee6cff773e981a0fbf821
DustinScharf <d.scharf.2002@gmail.com>
2021-11-30 04:06:03 +0100

    Reformatted comment

--- src/main/java/com/dustinscharf/fxglplayaround/GameApp.java
+++ src/main/java/com/dustinscharf/fxglplayaround/GameApp.java
@@ -65,6 +65,7 @@ public class GameApp extends GameApplication {
      * @param args launch arguments
      */
     public static void main(String[] args) {
-        launch(args); // launches the game app
+        // launches the game app
+        launch(args);
     }
 }
